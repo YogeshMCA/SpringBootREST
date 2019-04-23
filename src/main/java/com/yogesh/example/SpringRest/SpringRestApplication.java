327c401820657b8661d181858e5c61bf67b96f44
yogesh <yogee1389@gmail.com>
2019-04-23 10:35:31 +0530

    Latest Code

--- src/main/java/com/yogesh/example/SpringRest/SpringRestApplication.java
+++ src/main/java/com/yogesh/example/SpringRest/SpringRestApplication.java
@@ -11,29 +11,26 @@ import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
-<<<<<<< HEAD
+
 import com.fasterxml.jackson.databind.ser.impl.SimpleBeanPropertyFilter;
 import com.fasterxml.jackson.databind.ser.impl.SimpleFilterProvider;
 
-=======
->>>>>>> 74eb4a21ab0cda14a53a92735e1f83490a43175e
 @SpringBootApplication
 public class SpringRestApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringRestApplication.class, args);
 	}
-<<<<<<< HEAD
+
 	
 	@Bean
 	public SimpleFilterProvider simpleFilterProvider(){
 		return new SimpleFilterProvider();
 	}
 
-=======
+
 
 	//This method will set the default locale
->>>>>>> 74eb4a21ab0cda14a53a92735e1f83490a43175e
 	@Bean
 	public LocaleResolver localResolver() {
 		SessionLocaleResolver sessionResolver = new SessionLocaleResolver();
@@ -41,13 +38,10 @@ public class SpringRestApplication {
 		return sessionResolver;
 	}
 	
-<<<<<<< HEAD
+
 	@Bean
 	//By default method name will be the bean name
-=======
 	//This will read the messages.properties file
-	@Bean //By default method name will be the bean name
->>>>>>> 74eb4a21ab0cda14a53a92735e1f83490a43175e
 	public MessageSource messageSources() {
 		ResourceBundleMessageSource resource = new ResourceBundleMessageSource();
 		resource.setBasename("messages");
